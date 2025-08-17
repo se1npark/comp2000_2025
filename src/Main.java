b754a9fb80131779a56963349bd8f704d4608ac0
Nikhat <nikhatkr04@gmail.com>
2025-08-17 19:40:21 +1000

    push

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
-public class Main {
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+// testing pushing
+
+public class Main extends JFrame {
     public static void main(String[] args) throws Exception {
-        System.out.println("Red vs. Blue");
+      Main window = new Main();
+      window.run();
+    }
+
+    class Canvas extends JPanel {
+      public Canvas() {
+        setPreferredSize(new Dimension(720, 720));
+      }
+
+      @Override
+      public void paint(Graphics g) {
+        for (int i =10; i < 720; i += 35) {
+          for (int j = 10; j <720; j += 35){
+          g.drawRect(i, j, 35, 35);
+          }
+        }
+      }
+    }
+
+    private Main() {
+      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      Canvas canvas = new Canvas();
+      this.setContentPane(canvas);
+      this.pack();
+      this.setVisible(true);
+    }
+
+    public void run() {
+      while(true) {
+        repaint();
+      }
     }
 }
+//push test tenzin
